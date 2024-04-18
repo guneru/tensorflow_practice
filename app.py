02ff8ff57e0138e9ab5ad966a715cfc2cdce988d
구인모 <ttrr7eek@naver.com>
2024-04-18 13:27:32 +0900

    모델 완성

--- app.py
+++ app.py
@@ -18,3 +18,8 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(128, activation='tanh'),
     tf.keras.layers.Dense(1, activation='sigmoid'),
 ])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'] )
+
+model.fit( np.array(x데이터), np.array(y데이터), epochs=1000 )
+
