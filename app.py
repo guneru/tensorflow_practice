b6d14eef82b93afce0691d42d00d3b6f189ac9d1
구인모 <ttrr7eek@naver.com>
2024-04-18 13:22:16 +0900

    신경망모델 정의

--- app.py
+++ app.py
@@ -12,3 +12,9 @@ x데이터 = [ ]
 
 for i, rows in data.iterrows():
     x데이터.append([ rows['pregnant'], rows['plasma'], rows['pressure'], rows['thickness'], rows['insulin'], rows['bmi'], rows['pedigree'], rows['age'] ])
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation='tanh'),
+    tf.keras.layers.Dense(128, activation='tanh'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+])
