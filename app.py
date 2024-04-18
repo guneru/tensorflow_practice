4943ce349c31b7f20e40ae37754709813249dcc1
구인모 <ttrr7eek@naver.com>
2024-04-18 13:13:04 +0900

    x데이터 y데이터 만듬

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ data = pd.read_csv('pima-indians-diabetes3.csv')
 
 data = data.dropna()
 
+y데이터 = data['diabetes'].values
+
+x데이터 = [ ]
+
+for i, rows in data.iterrows():
+    x데이터.append([ rows['pregnant'], rows['plasma'], rows['pressure'], rows['thickness'], rows['insulin'], rows['bmi'], rows['pedigree'], rows['age'] ])
